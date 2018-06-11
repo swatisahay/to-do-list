09e4b1cf13efad1444bf406f9acfcc97cc4b6e7f
Jean Jia and Swati Sahay <swati@>
2018-06-11 15:28:44 -0700

    Edit scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 function toDoList(task, instruction, date) {
   this.task = task;
-  this.task = instruction;
-  this.task = date;
+  this.instruction = instruction;
+  this.date = date;
 }
-
+// toDoList.prototype.tasks = function() {
+//   return this.task + ", " + this.instruction + ", " + this.date;
+// }
 $(document).ready(function() {
   $("form#new-task").submit(function(event) {
     event.preventDefault();
@@ -14,11 +16,11 @@ $(document).ready(function() {
 
     var newTask = new toDoList(inputtedTask, inputtedInstruction, inputtedDate);
 
-    $("ul#task-list").append("<li><span class='toDo'>" + newTask.task + "</span></li>");
+    $("ul#task-list").append("<li><span class='toDo'>" + newTask.task+ "</span></li>");
 
     $(".toDo").last().click(function() {
-      $("#completed-list").show();
-      $("ul#completed-list").append("<li><span class='toDo'>" + newTask.task + "</span></li>");
+      $("#complete-instruction").show();
+      $("ul#completed-list").append("<li><span class='toDo'>" + newTask.tasks() + "</span></li>");
       $("li").last().click(function() {
         $(this).remove();
       });
@@ -30,6 +32,7 @@ $(document).ready(function() {
 
     $("input#new-task").val("");
     $("input#new-instruction").val("");
+    $("#date").val("");
 
   });
 });
